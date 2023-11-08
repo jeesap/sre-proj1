1620cf2eb169ad3e05051bdd4474196e7f24cc94
Jees Antony <53956834+jeesap@users.noreply.github.com>
2023-11-08 07:27:57 +0530

    Update ec2.tf

--- starter/terraform/ec2.tf
+++ starter/terraform/ec2.tf
@@ -11,7 +11,7 @@
    source             = "./modules/ec2"
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-00807e789908aecd8"
+   aws_ami            = "ami-0180be3ceaae655c8"
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
  }
